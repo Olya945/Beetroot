3e047f64ff44c7c5a032323cb6fbed5a89cb4893
Olya Petrenko <opyashchik160987@gmail.com>
2025-10-16 12:05:13 +0300

    HW_11

---
+++ hw_11/task1/read_file.py
@@ -0,0 +1,5 @@
+with open('myfile.txt', 'r', encoding='utf-8') as file:
+    content = file.read()
+    print(content)
+
+print('I did it!')
---
+++ hw_11/task1/write_file.py
@@ -0,0 +1,4 @@
+with open('myfile.txt', 'w', encoding='utf-8') as file:
+    file.write('Hello file world!\n')
+
+print("<The file named myfile.txt was crated>")
