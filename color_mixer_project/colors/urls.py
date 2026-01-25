0460cad2f503b26c20cb750ee55b47f1721f0ec1
Olya Petrenko <opyashchik160987@gmail.com>
2026-01-25 21:02:03 +0200

    HW_30 task2

--- color_mixer_project/colors/urls.py
+++ color_mixer_project/colors/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.color_list, name="color_list"),
-    path('recipes/', views.recipe_list, name='recipe_list'),
+    path("recipes/", views.recipe_list, name="recipe_list"),
+    path("mix/", views.what_can_i_mix, name="what_can_i_mix"),
 ]
--- color_mixer_project/colors/views.py
+++ color_mixer_project/colors/views.py
@@ -11,7 +11,30 @@ def recipe_list(request):
     recipes = Recipe.objects.all().order_by("result__name")
     return render(request, "colors/recipe_list.html", {"recipes": recipes})
 
-def color_mixer(request):
-    """Форма для змішування кольорів"""
+def what_can_i_mix(request):
+    """Показує що можна змішати з наявних кольорів"""
+    all_colors = Color.objects.all().order_by("name")
+    possible_recipes = []
+    selected_colors = []
 
-    pass
+    if request.method == "POST":
+        # Отримуємо ID вибраних кольорів
+        selected_color_ids = [int(id) for id in request.POST.getlist("colors")]
+        selected_colors = Color.objects.filter(id__in=selected_color_ids)
+
+        # Шукаємо рецепти де всі інгредієнти є в наявності
+        for recipe in Recipe.objects.all():
+            # Отримаємо інгредієнти цього рецепту
+            ingredients = recipe.recipe_ingredients.all()
+            ingredient_color_ids = [ing.color.id for ing in ingredients]
+
+            # Перевіряємо чи всі ID інгредієнтів є в наявності
+            if all(color_id in selected_color_ids for color_id in ingredient_color_ids):
+                possible_recipes.append(recipe)
+
+    return render(request, "colors/what_can_i_mix.html", {
+        "all_colors": all_colors,
+        "selected_colors": selected_colors,
+        "possible_recipes": possible_recipes
+    })
+
